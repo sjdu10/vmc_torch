fix bug in SR MPI calculation

vmc_torch/VMC.py
@@ -109,9 +109,11 @@ class VMC:
         for step in range(start, stop):
             self.step_count += 1
             # Compute the average energy and estimated energy gradient, meanwhile also record the amplitude_grad matrix
+
             # Use MPI for the sampling
+            # Only rank 0 collects the energy statistics, but all ranks must have the energy gradient
             state_MC_energy, state_MC_energy_grad = self._state.expect_and_grad(self._hamiltonian)
-            # Only rank 0 collects the energy statistics
+            state_MC_energy_grad = COMM.bcast(state_MC_energy_grad, root=0)
             
             MC_energy_stats['mean'].append(state_MC_energy['mean'])
             MC_energy_stats['error'].append(state_MC_energy['error'])
@@ -121,12 +123,19 @@ class VMC:
             preconditioned_grad = self.preconditioner(self._state, state_MC_energy_grad)
             if RANK == 0:
                 print('Variational step {}'.format(step))
+                print('Energy: {}, Err: {}, MAX gi: {}, Max SR gi: {}, Max param: {}'.format(
+                    state_MC_energy['mean'], 
+                    state_MC_energy['error'], 
+                    np.max(np.abs(state_MC_energy_grad)), 
+                    np.max(np.abs(preconditioned_grad.detach().numpy())), 
+                    np.max(np.abs(self._state.params_vec.detach().numpy())))
+                )
                 # Compute the new parameter vector
                 new_param_vec = self._optimizer.compute_update_params(self._state.params_vec, preconditioned_grad) # Subroutine: rank 0 computes new parameter vector based on the gradient
                 new_param_vec = new_param_vec.detach().numpy()
                 
                 self._state.reset() # Clear out the gradient of the state parameters
-                print('Energy: {}, Err: {}'.format(state_MC_energy['mean'], state_MC_energy['error']))
+                
                 
                 if tmpdir is not None and save:
                     # with open(tmpdir, 'a') as f:

vmc_torch/optimizer.py
@@ -27,7 +27,7 @@ class Preconditioner:
 class TrivialPreconditioner(Preconditioner):
     """Trivial preconditioner that does nothing."""
     def __call__(self, state, grad):
-        return grad
+        return torch.tensor(grad, dtype=torch.float32)
     
 class SR(Preconditioner):
     """
@@ -67,15 +67,23 @@ class SR(Preconditioner):
             return torch.tensor(dp, dtype=torch.float32)
 
         if self.dense:
-            logamp_grad_matrix, mean_logamp_grad = state.get_logamp_grad_matrix()
+            # Send the logamp_grad_matrix to rank 0 to form the dense S matrix
+            local_logamp_grad_matrix, mean_logamp_grad = state.get_logamp_grad_matrix()
             logamp_grad_matrix_list = COMM.gather(local_logamp_grad_matrix, root=0)
-            if energy_grad is None:
+
+            if energy_grad is None or RANK != 0:
                 return torch.zeros(state.Np, dtype=torch.float32)
-            logamp_grad_matrix = np.concatenate(logamp_grad_matrix_list, axis=1)
+            
+            # Convert to list of numpy arrays
+            logamp_grad_matrix_list = [logamp_grad_vec for logamp_grad_matrix in logamp_grad_matrix_list for logamp_grad_vec in logamp_grad_matrix.T ]
+            
+            # Convert the list of vectors to a single matrix
+            logamp_grad_matrix = np.array(logamp_grad_matrix_list)
+
             if type(logamp_grad_matrix) is torch.Tensor:
                 logamp_grad_matrix = logamp_grad_matrix.detach().numpy()
             # form the dense S matrix
-            S = np.mean([np.outer(logamp_grad, logamp_grad.conj()) for logamp_grad in logamp_grad_matrix.T], axis=0)
+            S = np.mean([np.outer(logamp_grad, logamp_grad.conj()) for logamp_grad in logamp_grad_matrix], axis=0)
             S -= np.outer(mean_logamp_grad, mean_logamp_grad.conj())
             R = S + self.diag_eta*np.eye(S.shape[0])
             R = csr_matrix(R)
@@ -91,14 +99,15 @@ class SR(Preconditioner):
                 This is at the cost of having to broadcast the energy_grad to all ranks
                 and solve the linear equation locally in every rank."""
                 local_logamp_grad_matrix, mean_logamp_grad = state.get_logamp_grad_matrix()
-                energy_grad = COMM.bcast(energy_grad, root=0)
+                if energy_grad is None:
+                    energy_grad = COMM.bcast(energy_grad, root=0)
                 def R_dot_x(x, eta=1e-6):
                     x_out_local = np.zeros_like(x)
-                    # for i in range(local_logamp_grad_matrix.shape[1]):
-                    #     x_out_local += np.dot(local_logamp_grad_matrix[:, i], x)*local_logamp_grad_matrix[:, i]
+                    for i in range(local_logamp_grad_matrix.shape[1]):
+                        x_out_local += np.dot(local_logamp_grad_matrix[:, i], x)*local_logamp_grad_matrix[:, i]
                     # use matrix multiplication for speedup
-                    x_out_local = np.dot(local_logamp_grad_matrix, np.dot(local_logamp_grad_matrix.T, x))
-                    x_out = COMM.allreduce(x_out_local, op=MPI.SUM)
+                    # x_out_local = np.dot(local_logamp_grad_matrix, np.dot(local_logamp_grad_matrix.T, x))
+                    x_out = COMM.allreduce(x_out_local, op=MPI.SUM)/state.Ns
                     x_out -= np.dot(mean_logamp_grad, x)*mean_logamp_grad
                     return x_out + eta*x
                 n = state.Np

vmc_torch/vmc_run.py
@@ -42,7 +42,7 @@ RANK = COMM.Get_rank()
 # Hamiltonian parameters
 Lx = int(4)
 Ly = int(4)
-symmetry = 'U1'
+symmetry = 'Z2'
 t = 1.0
 V = 1.0
 N_f = int(Lx*Ly/2)-2
@@ -50,7 +50,7 @@ H, hi, graph = square_lattice_spinless_Fermi_Hubbard(Lx, Ly, t, V, N_f)
 
 # TN parameters
 D = 4
-chi = 4
+chi = 32
 
 # Load PEPS
 skeleton = pickle.load(open(f"../data/{Lx}x{Ly}/t={t}_V={V}/N={N_f}/{symmetry}/D={D}/peps_skeleton.pkl", "rb"))
@@ -59,8 +59,8 @@ peps = qtn.unpack(peps_params, skeleton)
 peps.apply_to_arrays(lambda x: torch.tensor(x, dtype=torch.float32))
 
 # VMC parameters
-N_samples = 128
-N_samples = N_samples - N_samples % SIZE + SIZE
+N_samples = 32
+N_samples = N_samples - N_samples % SIZE + SIZE - 1
 
 model = fTNModel(peps, max_bond=chi)
 # model = fTN_NNiso_Model(peps, max_bond=chi, nn_hidden_dim=8, nn_eta=1e-3)
@@ -68,7 +68,7 @@ model = fTNModel(peps, max_bond=chi)
 # model = fTN_Transformer_Model(
 #     peps, 
 #     max_bond=chi, 
-#     nn_eta=0.0, 
+#     nn_eta=1e-1, 
 #     d_model=8, 
 #     nhead=2, 
 #     num_encoder_layers=2, 
@@ -92,12 +92,12 @@ if init_step != 0:
     except:
         model.load_params(saved_model_params_vec)
 
-# optimizer = SignedSGD(learning_rate=0.05)
-optimizer = SGD(learning_rate=0.05)
+optimizer = SignedSGD(learning_rate=1e-3)
+# optimizer = SGD(learning_rate=0.05)
 sampler = MetropolisExchangeSampler(hi, graph, N_samples=N_samples, burn_in_steps=1)
 # sampler = None
 variational_state = Variational_State(model, hi=H.hilbert, sampler=sampler)
-preconditioner = SR(dense=False, exact=True if sampler is None else False, use_MPI4Solver=True, diag_eta=0.01, iter_step=1e2)
+preconditioner = SR(dense=False, exact=True if sampler is None else False, use_MPI4Solver=True, diag_eta=1e-3, iter_step=1e4)
 # preconditioner = TrivialPreconditioner()
 vmc = VMC(H, variational_state, optimizer, preconditioner)
 