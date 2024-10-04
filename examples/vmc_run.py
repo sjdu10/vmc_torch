171724adf22df60a90f97c91da5c28ec1f5d5038
sijingdu <sdu2@caltech.edu>
2024-10-04 03:47:42 -0700

    add example models and run script

---
+++ examples/vmc_run.py
@@ -0,0 +1,116 @@
+import numpy as np
+from quimb.utils import progbar as Progbar
+from mpi4py import MPI
+import pickle
+import os
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
+# torch
+from torch.nn.parameter import Parameter
+import torch
+import torch.nn as nn
+torch.autograd.set_detect_anomaly(False)
+
+# quimb
+import quimb as qu
+import quimb.tensor as qtn
+import symmray as sr
+import autoray as ar
+from autoray import do
+
+from vmc_torch.model import SlaterDeterminant, NeuralBackflow, FFNN, NeuralJastrow
+from vmc_torch.model import init_weights_xavier, init_weights_kaiming, init_weights_to_zero
+from vmc_torch.sampler import MetropolisExchangeSampler
+from vmc_torch.variational_state import Variational_State
+from vmc_torch.optimizer import TrivialPreconditioner, SignedSGD, SGD, SR
+from vmc_torch.VMC import VMC
+from vmc_torch.hamiltonian import square_lattice_spinless_Fermi_Hubbard
+from vmc_torch.torch_utils import SVD,QR
+
+# Register safe SVD and QR functions to torch
+ar.register_function('torch','linalg.svd',SVD.apply)
+ar.register_function('torch','linalg.qr',QR.apply)
+from vmc_torch.global_var import DEBUG
+
+dtype=torch.float64
+
+COMM = MPI.COMM_WORLD
+SIZE = COMM.Get_size()
+RANK = COMM.Get_rank()
+
+"""Define the Hamiltonian"""
+Lx = int(4)
+Ly = int(4)
+symmetry = 'Z2'
+t = 1.0
+V = 1.0
+N_f = int(Lx*Ly/2)-2
+H, hi, graph = square_lattice_spinless_Fermi_Hubbard(Lx, Ly, t, V, N_f)
+
+"""Choose the torch model (you can create your own model of course)"""
+# model = SlaterDeterminant(hi)
+# model=NeuralBackflow(hi, param_dtype=dtype, hidden_dim=hi.size)
+# model = NeuralJastrow(hi, param_dtype=dtype, hidden_dim=hi.size)
+model = FFNN(hi, hidden_dim=2*hi.size)
+model_names = {
+    SlaterDeterminant: 'SlaterDeterminant',
+    NeuralBackflow: 'NeuralBackflow',
+    FFNN: 'FFNN',
+    NeuralJastrow: 'NeuralJastrow',
+}
+model_name = model_names.get(type(model), 'UnknownModel')
+
+
+"""Choose the initialization method for NN parameters"""
+# model.apply(init_weights_to_zero)
+model.apply(init_weights_xavier)
+
+
+"""Potentially load a pre-trained model"""
+init_step = 0
+total_steps = 200
+if init_step != 0:
+    saved_model_params = torch.load(f'../data/{Lx}x{Ly}/t={t}_V={V}/N={N_f}/{symmetry}/{model_name}/model_params_step{init_step}.pth')
+    saved_model_state_dict = saved_model_params['model_state_dict']
+    saved_model_params_vec = torch.tensor(saved_model_params['model_params_vec'])
+    try:
+        model.load_state_dict(saved_model_state_dict)
+    except:
+        model.load_params(saved_model_params_vec)
+
+
+"""Set VMC sample size"""
+N_samples = 2**10
+N_samples = N_samples - N_samples % SIZE + SIZE - 1
+
+
+"""Choose the sampler""" 
+sampler = MetropolisExchangeSampler(hi, graph, N_samples=N_samples, burn_in_steps=16, reset_chain=False, random_edge=True, dtype=dtype)
+# sampler = None
+
+
+"""Choose the optimizer and preconditioner"""
+# optimizer = SignedSGD(learning_rate=0.05)
+optimizer = SGD(learning_rate=0.05)
+preconditioner = SR(dense=False, exact=True if sampler is None else False, use_MPI4Solver=True, diag_eta=0.05, iter_step=1e5, dtype=dtype)
+# preconditioner = TrivialPreconditioner()
+
+
+"""Create the variational state object"""
+variational_state = Variational_State(model, hi=H.hilbert, sampler=sampler, dtype=dtype)
+
+
+"""Create the VMC object"""
+vmc = VMC(H, variational_state, optimizer, preconditioner)
+
+
+if __name__ == "__main__":
+    
+    """Run the code by command: `mpirun -np 10 python vmc_run.py`
+    you can change the number 10 to the number of MPI processes you want to use"""
+    
+    torch.autograd.set_detect_anomaly(False)
+    os.makedirs(f'../data/{Lx}x{Ly}/t={t}_V={V}/N={N_f}/{symmetry}/{model_name}/', exist_ok=True)
+    vmc.run(init_step, init_step+total_steps, tmpdir=f'../data/{Lx}x{Ly}/t={t}_V={V}/N={N_f}/{symmetry}/{model_name}/')
+
