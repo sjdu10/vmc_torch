1616d83dff14797826b55a2658f08fcbe905003b
sijingdu <sdu2@caltech.edu>
2024-10-21 22:56:53 -0700

    Remove ignored files from the repository

--- vmc_torch/experiment/mpi_check.py
+++
@@ -1,7 +0,0 @@
-from mpi4py import MPI
-import mpi4py
-COMM = MPI.COMM_WORLD
-SIZE = COMM.Get_size()
-RANK = COMM.Get_rank()
-
-print(SIZE, RANK)
--- vmc_torch/global_var.py
+++
@@ -1,6 +0,0 @@
-DEBUG = False
-TIME_PROFILING = False
-
-def set_debug(debug):
-    global DEBUG
-    DEBUG = debug
