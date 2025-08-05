fix bugs in reusable fPEPS alg: in sampler when accept rate<0.1 need to reset chain and CLEAR THE TN ENV CACHE; in tn gradient calculation remember to take into account the peps.exponent

vmc_torch/experiment/tn_model.py
@@ -2099,6 +2099,7 @@ class fTNModel_reuse(wavefunctionModel):
         # self.skeleton.exponent = 0
 
         # Flatten the dictionary structure and assign each parameter as a part of a ModuleDict
+        # NOTE: pytorch nn.ParameterDict automatically sorts the keys (as sorted(dict))
         self.torch_tn_params = nn.ModuleDict({
             str(tid): nn.ParameterDict({
                 str(sector): nn.Parameter(data)
@@ -2106,6 +2107,7 @@ class fTNModel_reuse(wavefunctionModel):
             })
             for tid, blk_array in params.items()
         })
+        self.tn_param_key_id = 'torch_tn_params.{}.{}'
 
         # Get symmetry
         self.symmetry = ftn.arrays[0].symmetry
@@ -2142,6 +2144,16 @@ class fTNModel_reuse(wavefunctionModel):
         """
         if ordering == 'snake':
             return (site // self.Ly, site % self.Ly)
+        else:
+            raise ValueError(f"Unsupported ordering: {ordering}")
+    
+    def from_2dsite_to_1dsite(self, site, ordering='snake'):
+        """
+            Convert a 2d site index to a 1d site index.
+            site: (row, col) tuple
+        """
+        if ordering == 'snake':
+            return site[0] * self.Ly + site[1]
         else:
             raise ValueError(f"Unsupported ordering: {ordering}")
     
@@ -2369,10 +2381,12 @@ class fTNModel_reuse(wavefunctionModel):
             Get the local tensors for the given tensor ids and configuration.
             tids: a list of tensor ids. list of int.
             config: the input configuration.
+            sites: a list of 1d site/2d site indices corresponding to the tids.
         """
         # first pick out the tensor parameters and form the local tn parameters vector
         local_ts_params = {}
-        tids = self.from_1d_sites_to_tids(sites)
+        # tids = self.from_1d_sites_to_tids(sites)
+        tids = self.from_1d_sites_to_tids([self.from_2dsite_to_1dsite(site) for site in sites]) if isinstance(sites[0], tuple) else self.from_1d_sites_to_tids(sites)
         for tid in tids:
             local_ts_params[tid] = {
                 ast.literal_eval(sector): data
@@ -2380,7 +2394,8 @@ class fTNModel_reuse(wavefunctionModel):
             }
         
         # Get sites corresponding to the tids
-        sites_2d = [self.from_1dsite_to_2dsite(site) for site in sites]
+        sites_1d = [self.from_2dsite_to_1dsite(site) for site in sites] if isinstance(sites[0], tuple) else sites
+        sites_2d = sites if isinstance(sites[0], tuple) else [self.from_1dsite_to_2dsite(site) for site in sites]
 
         # Select the corresponding tensor skeleton
         local_ts_skeleton = self.skeleton.select([self.skeleton.site_tag_id.format(*site) for site in sites_2d], which='any')
@@ -2389,7 +2404,7 @@ class fTNModel_reuse(wavefunctionModel):
         local_ftn = qtn.unpack(local_ts_params, local_ts_skeleton)
 
         # Fix the physical indices
-        return local_ftn.fix_phys_inds(sites_2d, config[sites])
+        return local_ftn.fix_phys_inds(sites_2d, config[sites_1d])
     
     def get_amp_tn(self, config, reconstruct=False):
 
@@ -2463,15 +2478,14 @@ class fTNModel_reuse(wavefunctionModel):
                     self.cache_env_x(amp_tn, x_i)
                     # self.cache_env_y(amp, x_i)
                     self.config_ref = x_i
-                    # config_2d = self.from_1d_to_2d(x_i)
-                    # key_bot = ('xmax', tuple(torch.cat(tuple(config_2d[self.Lx//2:].to(torch.int))).tolist()))
-                    # key_top = ('xmin', tuple(torch.cat(tuple(config_2d[:self.Lx//2].to(torch.int))).tolist()))
-                    # amp_bot = self.env_x_cache[key_bot]
-                    # amp_top = self.env_x_cache[key_top]
-                    amp_val = amp_tn.contract()
-                    # print(f'Rank {RANK}: Exact contraction for gradient backpropagation')
-                    # amp_val = (amp_bot|amp_top).contract()*10**(self.skeleton.exponent) # quimb cannot address the cached exponent automatically when TN reuses the cached environment, so we need to multiply it manually
-                    
+                    config_2d = self.from_1d_to_2d(x_i)
+                    key_bot = ('xmax', tuple(torch.cat(tuple(config_2d[self.Lx//2:].to(torch.int))).tolist()))
+                    key_top = ('xmin', tuple(torch.cat(tuple(config_2d[:self.Lx//2].to(torch.int))).tolist()))
+                    amp_bot = self.env_x_cache[key_bot]
+                    amp_top = self.env_x_cache[key_top]
+                    amp_val = (amp_bot|amp_top).contract()*10**(self.skeleton.exponent) # quimb cannot address the cached exponent automatically when TN reuses the cached environment, so we need to multiply it manually
+                    if DEBUG:
+                        amp_val = psi.get_amp(x_i).contract()
 
                 else:
                     if self.env_x_cache is None and self.env_y_cache is None:
@@ -2520,13 +2534,85 @@ class fTNModel_reuse(wavefunctionModel):
             
             if self.debug:
                 amp_val_exact = psi.get_amp(x_i).contract()
-                print(f'Reused Amp val: {amp_val}, Exact Amp val: {amp_val_exact}, Rel error: {torch.abs(amp_val_exact - amp_val) / torch.abs(amp_val_exact)}')
-                self.debug_amp_cache.append((amp_val, amp_val_exact, torch.abs(amp_val_exact - amp_val) / torch.abs(amp_val_exact)))
+                if (amp_val - amp_val_exact).abs() > 1e-4:
+                    print(f'Warning: Reused Amp val and Exact Amp val differ significantly! Reused Amp val: {amp_val}, Exact Amp val: {amp_val_exact}, Rel error: {torch.abs(amp_val_exact - amp_val) / torch.abs(amp_val_exact)}')
+                
 
             batch_amps.append(amp_val)
 
         # Return the batch of amplitudes stacked as a tensor
         return torch.stack(batch_amps)
+    
+    def get_grad(self):
+        """"Compute the amplitude gradient by contracting TN without the on-site tensor."""
+        if self.debug:
+            print('Computing the amplitude gradient by contracting TN without the on-site tensor.')
+        self.zero_grad()
+        sampled_x = self.config_ref
+        amp_tn = self.amp_ref
+        index_map = {0: 0, 1: 1, 2: 1, 3: 0}
+        array_map = {
+            0: torch.tensor([1.0, 0.0]),
+            1: torch.tensor([1.0, 0.0]),
+            2: torch.tensor([0.0, 1.0]),
+            3: torch.tensor([0.0, 1.0])
+        }
+        config_2d = self.from_1d_to_2d(sampled_x)
+        for tid, model_ts_params in self.torch_tn_params.items():
+            # utils numbers
+            site_2d = self.from_1dsite_to_2dsite(int(tid))
+
+            p_ind = self.skeleton.site_ind_id.format(*site_2d)
+            p_ind_order = self.skeleton.tensor_map[int(tid)].inds.index(p_ind)
+            on_site_config = int(sampled_x[int(tid)])
+            on_site_config_parity = index_map[on_site_config]
+            site_tag = self.skeleton.site_tag_id.format(*site_2d)
+            input_vec = array_map[on_site_config]
+
+            ts0 = amp_tn.select(site_tag).contract()
+            ts_params = ts0.get_params()
+
+            # Reuse cached environment to compute grad_ts
+            row_id = site_2d[0]
+            # select the cached_env on both sides of the site tensor along the row
+            rows_above = list(range(row_id+1, self.Lx))
+            rows_below = list(range(0, row_id))
+            cached_amp_tn_above = self.env_x_cache[('xmax', tuple(torch.cat(tuple(config_2d[rows_above].to(torch.int))).tolist()))] if rows_above else qtn.TensorNetwork([])
+            cached_amp_tn_below = self.env_x_cache[('xmin', tuple(torch.cat(tuple(config_2d[rows_below].to(torch.int))).tolist()))] if rows_below else qtn.TensorNetwork([])
+            within_row_sites = list((site_2d[0], col_id) for col_id in range(self.Ly) if col_id != site_2d[1])
+            within_row_hole_tn = self.get_local_amp_tensors(within_row_sites, config=sampled_x)
+            grad_ts = (within_row_hole_tn | cached_amp_tn_above | cached_amp_tn_below).contract()
+
+            # # Exact contraction of the TN without the site tensor, naive calculation, expensive baseline.
+            # ts_left = [ts for ts in amp_tn.tensors if site_tag not in ts.tags]
+            # tn_left = qtn.TensorNetwork(ts_left)
+            # grad_ts = tn_left.contract()
+
+            grad_ts.data.phase_sync(inplace=True)
+
+            # Back propagate through the final contraction
+            ts0.apply_to_arrays(lambda x: torch.tensor(x, dtype=self.param_dtype, requires_grad=True))
+            grad_ts.apply_to_arrays(lambda x: torch.tensor(x, dtype=self.param_dtype, requires_grad=False))
+            amp_temp = (ts0|grad_ts).contract()*10**(self.skeleton.exponent)
+            amp_temp.backward()
+
+            grad_ts_backprop_params = ts0.get_params().copy() # correct gradients
+            for blk, ts_temp in ts0.get_params().items():
+                grad_ts_backprop_params[blk] = ts_temp.grad
+
+            # select the remaining sectors in model_ts_params
+            remaining_sliced_sectors = [sector for sector in sorted(ts_params)]
+            remaining_sectors = [sector + (on_site_config_parity,) for sector in sorted(ts_params)]
+
+            for sliced_blk, blk in zip(remaining_sliced_sectors, remaining_sectors):
+                data = model_ts_params[str(blk)]
+                select_index = torch.argmax(input_vec).item()
+                slicer = [slice(None)] * data.ndim
+                slicer[p_ind_order] = select_index
+                reconstructed_grad_tensor = torch.zeros_like(data)
+                reconstructed_grad_tensor[tuple(slicer)] = grad_ts_backprop_params[sliced_blk]
+                data.grad = reconstructed_grad_tensor
+
 
 
 class fTNModel_test(wavefunctionModel):

vmc_torch/fermion_utils.py
@@ -367,8 +367,26 @@ class fPEPS(qtn.PEPS):
                 new_charge_sec_data_dict = {} # new dictionary to store the data of the contracted f-tensor
                 for charge_blk, data in charge_sec_data_dict.items():
                     if charge_blk[phys_ind_order] == charge:
-                        # new_data = data @ input_vec # BUG: This is not correct, should contract with the correct tensor index
-                        new_data = do('tensordot', data, input_vec, axes=([phys_ind_order], [0])) # new tensor for this charge block
+                        # ------------------------------------------------------------------
+                        # Replacement for: 
+                        # new_data = do('tensordot', data, input_vec, axes=([phys_ind_order], [0]))
+                        # ------------------------------------------------------------------
+
+                        # 1. Determine which index to select (0 or 1) from the input vector.
+                        #    `argmax` finds the position of the '1.0'.
+                        select_index = torch.argmax(input_vec).item()
+
+                        # 2. Build the slicer tuple dynamically.
+                        #    This creates a list of `slice(None)` (which is equivalent to `:`)
+                        #    and inserts the `select_index` at the correct position.
+                        slicer = [slice(None)] * data.ndim
+                        slicer[phys_ind_order] = select_index
+
+                        # 3. Apply the slice to get the new data.
+                        new_data = data[tuple(slicer)]
+                        # ------------------------------------------------------------------
+                        # new_data = do('tensordot', data, input_vec, axes=([phys_ind_order], [0])) # new tensor for this charge block
+
                         new_charge_blk = charge_blk[:phys_ind_order] + charge_blk[phys_ind_order + 1:] # new charge block
                         new_charge_sec_data_dict[new_charge_blk] = new_data # new charge block and its corresponding data in a dictionary
 

vmc_torch/torch_utils.py
@@ -179,8 +179,8 @@ class QR(torch.autograd.Function):
             raise ValueError
         inds = torch.abs(diag) < 1e-6
         if sum(inds) > 0: # rank deficient, revert to svd
-            if DEBUG:
-                print(f"QRforward: rank deficient, using SVD, {sum(inds)} out of {len(inds)} diagonals are zero: {R}")
+            # if DEBUG:
+            #     print(f"QRforward: rank deficient, using SVD, {sum(inds)} out of {len(inds)} diagonals are zero: {R}")
             U,S,Vh = SVDforward(A)
             SVh = S.reshape((S.size(0),1)) * Vh
             self.save_for_backward(U, S, Vh)
@@ -197,8 +197,8 @@ class QR(torch.autograd.Function):
     def backward(self, dQ, dR):
         M1,M2,M3 = self.saved_tensors
         if len(M2.size())==1: # rank-deficient, do svd
-            if DEBUG:
-                print("QRbackward: rank deficient, using SVD")
+            # if DEBUG:
+            #     print("QRbackward: rank deficient, using SVD")
             U,S,Vh = M1,M2,M3
             dU,dSVh = dQ,dR
             dS = torch.diag(dSVh @ Vh.t())

vmc_torch/variational_state.py
@@ -6,6 +6,8 @@ import torch
 # quimb
 from autoray import do
 
+import vmc_torch
+
 from .global_var import DEBUG, set_debug, AMP_CACHE_SIZE, GRAD_CACHE_SIZE
 from .utils import tensor_aware_lru_cache
 
@@ -111,19 +113,32 @@ class Variational_State:
             x = torch.tensor(np.asarray(x), dtype=self.dtype)
         self.reset()
         amp = self.vstate_func(x)
-        # try:
-        amp.backward(retain_graph=retain_graph)
 
+        if type(self.vstate_func) == vmc_torch.experiment.tn_model.fTNModel_reuse:
+            self.vstate_func.get_grad()
+            vec_log_grad = self.vstate_func.params_grad_to_vec()/amp
+
+            if DEBUG:
+                self.reset()
+                amp.backward(retain_graph=retain_graph)
+                autodiff_grad = self.vstate_func.params_grad_to_vec()/amp
+                if torch.norm(vec_log_grad - autodiff_grad) / torch.norm(autodiff_grad) > 1e-3:
+                    print(f"Rank {RANK} amplitude: {amp}, rel grad err: {torch.norm(vec_log_grad - autodiff_grad) / torch.norm(autodiff_grad)}")
+                    print(f'autodiff grad norm: {torch.norm(autodiff_grad)}')
+                    print(f'vec_log_grad norm: {torch.norm(vec_log_grad)}')
+        else:
+            # Backpropagate the amplitude to compute the gradient
+            amp.backward(retain_graph=retain_graph)
+            vec_log_grad = self.vstate_func.params_grad_to_vec()/amp
+
+        # try:
         # except:
         #     print(f"Amplitude backward failed, returning zero gradient.")
         #     print(f"Rank {RANK} amplitude: {amp}")
         #     # amp is 0
         #     self.reset()
         #     return amp, torch.zeros((self.Np,), dtype=self.dtype)
-        vec_log_grad = self.vstate_func.params_grad_to_vec()/amp
-        if DEBUG:
-            if torch.mean(torch.abs(vec_log_grad)) > 1e3:
-                print(f"Rank {RANK} amplitude: {amp}, mean log grad: {torch.mean(torch.abs(vec_log_grad))}")
+
         # Clear the gradient
         self.reset()
         return amp, vec_log_grad