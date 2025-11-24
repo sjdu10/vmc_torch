1.add spinless reusable PEPS model and exchange sampler; 2.move the QR_tao_direct forward pass logics of adding diagonal shift to after fixing the diag signs

examples/SU_Ising.py
@@ -8,8 +8,6 @@ ndim = 2
 Lx = 4
 Ly = 2
 pbc = False
-total_sz = 0.0
-print(f"Total Sz = {total_sz}")
 D = 2
 
 # PEPS tensor network
@@ -20,11 +18,12 @@ J=1.0
 h=0.5
 
 # ED
-ham_dense =  qu.ham_heis_2D(Lx, Ly, j=(J,0,0), bz=h, cyclic=pbc)
-evals, evecs = scipy.linalg.eigh(ham_dense)
-exact_energy = evals[0]/(Lx*Ly)
-print(f"Exact ground state energy per site: {exact_energy}")
-
+if Lx*Ly <= 16:  # Only perform ED for small systems
+    ham_dense =  qu.ham_heis_2D(Lx, Ly, j=(J,0,0), bz=h, cyclic=pbc)
+    evals, evecs = scipy.linalg.eigh(ham_dense)
+    exact_energy = evals[0]/(Lx*Ly)
+    print(f"Exact ground state energy per site: {exact_energy}")
+    
 ham = qtn.ham_2d_ising(Lx=Lx, Ly=Ly, j=J, bx=h)
 su = qtn.tensor_arbgeom_tebd.SimpleUpdateGen(
     psi, 

examples/SU_heis.py
@@ -8,8 +8,6 @@ ndim = 2
 Lx = 4
 Ly = 2
 pbc = False
-total_sz = 0.0
-print(f"Total Sz = {total_sz}")
 D = 2
 
 # PEPS tensor network
@@ -18,10 +16,11 @@ J=1.0
 ham = qtn.ham_2d_heis(Lx=Lx, Ly=Ly, j=J)
 
 # ED
-ham_dense =  qu.ham_heis_2D(Lx, Ly, j=J, cyclic=pbc)
-evals, evecs = scipy.linalg.eigh(ham_dense)
-exact_energy = evals[0]/(Lx*Ly)
-print(f"Exact ground state energy per site: {exact_energy}")
+if Lx*Ly <= 16:
+    ham_dense =  qu.ham_heis_2D(Lx, Ly, j=J, cyclic=pbc)
+    evals, evecs = scipy.linalg.eigh(ham_dense)
+    exact_energy = evals[0]/(Lx*Ly)
+    print(f"Exact ground state energy per site: {exact_energy}")
 
 su = qtn.tensor_arbgeom_tebd.SimpleUpdateGen(
     psi, 

examples/vmc_run_example_heis_reuse.py
@@ -13,12 +13,12 @@ import torch
 import quimb.tensor as qtn
 import autoray as ar
 
-from vmc_torch.experiment.tn_model import PEPS_model, PEPS_NN_Model, init_weights_to_zero, PEPS_NNproj_Model, PEPS_delocalized_Model
-from vmc_torch.sampler import MetropolisSamplerSpinless
+from vmc_torch.experiment.tn_model import PEPS_model_reuse, PEPS_NN_Model, init_weights_to_zero, PEPS_NNproj_Model, PEPS_delocalized_Model
+from vmc_torch.sampler import MetropolisExchangeSamplerSpinless_2D_reusable
 from vmc_torch.variational_state import Variational_State
-from vmc_torch.optimizer import SGD, SR
+from vmc_torch.optimizer import TrivialPreconditioner, SignedSGD, SGD, SR
 from vmc_torch.VMC import VMC
-from vmc_torch.hamiltonian_torch import spin_transverse_Ising_square_lattice_torch
+from vmc_torch.hamiltonian_torch import spin_Heisenberg_square_lattice_torch
 from vmc_torch.torch_utils import SVD,QR
 
 # Register safe SVD and QR functions to torch
@@ -34,8 +34,7 @@ RANK = COMM.Get_rank()
 Lx = int(4)
 Ly = int(2)
 J = 1.0
-h = 0.5
-H = spin_transverse_Ising_square_lattice_torch(Lx, Ly, J, -h, pbc=False, total_sz=None) 
+H = spin_Heisenberg_square_lattice_torch(Lx, Ly, J, total_sz=0) 
 graph = H.graph
 
 """TNS parameters"""
@@ -44,19 +43,19 @@ chi = 4*D
 dtype=torch.float64
 
 """Load Simple Update PEPS as the initial state"""
-skeleton = pickle.load(open(f"./example_data/{Lx}x{Ly}/J={J}_h={h}/D={D}/peps_skeleton.pkl", "rb"))
-peps_params = pickle.load(open(f"./example_data/{Lx}x{Ly}/J={J}_h={h}/D={D}/peps_su_params.pkl", "rb"))
+skeleton = pickle.load(open(f"./example_data/{Lx}x{Ly}/J={J}/D={D}/peps_skeleton.pkl", "rb"))
+peps_params = pickle.load(open(f"./example_data/{Lx}x{Ly}/J={J}/D={D}/peps_su_params.pkl", "rb"))
 peps = qtn.unpack(peps_params, skeleton)
 peps.apply_to_arrays(lambda x: torch.tensor(x, dtype=dtype))
 
 
 """Choose the torch model (you can create your own model of course)"""
-model = PEPS_model(peps, max_bond=chi)
+model = PEPS_model_reuse(peps, max_bond=chi)
 # model = PEPS_delocalized_Model(peps, max_bond=chi, diag=False)
 # model = PEPS_NN_Model(peps, max_bond=chi_nn, nn_eta=1.0, nn_hidden_dim=Lx*Ly)
 # model = PEPS_NNproj_Model(peps, max_bond=chi_nn, nn_eta=1.0, nn_hidden_dim=Lx*Ly)
 model_names = {
-    PEPS_model: 'PEPS',
+    PEPS_model_reuse: 'PEPS_reuse',
     PEPS_delocalized_Model: 'PEPS_delocalized_diag='+str(model.diag) if isinstance(model, PEPS_delocalized_Model) else None,
     PEPS_NN_Model: 'PEPS_NN',
     PEPS_NNproj_Model: 'PEPS_NNproj'
@@ -85,7 +84,7 @@ N_samples = int(5e3)
 N_samples = N_samples - N_samples % SIZE + SIZE - 1
 
 """Choose the sampler""" 
-sampler = MetropolisSamplerSpinless(H.hilbert, graph, N_samples=N_samples, burn_in_steps=20, reset_chain=True, random_site=False, equal_partition=False, dtype=dtype)
+sampler = MetropolisExchangeSamplerSpinless_2D_reusable(H.hilbert, graph, N_samples=N_samples, burn_in_steps=20, reset_chain=False, random_edge=False, equal_partition=False, dtype=dtype)
 
 """Choose the optimizer and preconditioner"""
 optimizer = SGD(learning_rate=1e-2)

vmc_torch/VMC.py
@@ -233,7 +233,14 @@ class VMC:
                         np.max(np.abs(self._state.params_vec.detach().numpy())))
                     )
                 except Exception:
-                    print('Energy: {}, Err: {}'.format(state_MC_energy['mean'], state_MC_energy['error']))
+                    print(
+                        "Energy: {}, Err: {} \nMAX gi: {}, Max SR gi (optional): {}".format(
+                            state_MC_energy["mean"],
+                            state_MC_energy["error"],
+                            np.max(np.abs(state_MC_loss_grad.detach().numpy())),
+                            np.max(np.abs(preconditioned_grad.detach().numpy())),
+                        )
+                    )
                 pbar.update(1)
                 # Compute the new parameter vector
                 new_param_vec = self._optimizer.compute_update_params(self._state.params_vec, preconditioned_grad) # Subroutine: rank 0 computes new parameter vector based on the gradient

vmc_torch/torch_utils.py
@@ -269,13 +269,13 @@ class QR_tao_direct(torch.autograd.Function):
     def forward(self, A):
         Q, R = torch.linalg.qr(A)
         diag = R.diag()
-        if torch.any(diag.abs() < 1e-7): # rank deficient, add diagonal regularization NOTE: this will perturb the forward result
-            # note that R can be rectangular here
-            R = R + torch.eye(R.size(0), R.size(1), dtype=R.dtype, device=R.device) * 1e-7
         if fix_sign:
             sign = torch.sign(diag).reshape((1,-1))
             Q = Q * sign
             R = R * sign.t()
+        if torch.any(R.diag().abs() < 1e-7): # rank deficient, add diagonal regularization NOTE: this will perturb the forward result
+            # note that R can be rectangular here
+            R = R + torch.eye(R.size(0), R.size(1), dtype=R.dtype, device=R.device) * 1e-7
         self.save_for_backward(A, Q, R)
         return Q, R
 

vmc_torch/variational_state.py
@@ -118,7 +118,7 @@ class Variational_State:
             x = torch.tensor(np.asarray(x), dtype=self.dtype)
         self.reset()
         amp = self.vstate_func(x, grad=True)
-        if isinstance(self.vstate_func, fTNModel_reuse):
+        if isinstance(self.vstate_func, fTNModel_reuse) or isinstance(self.vstate_func, vmc_torch.experiment.tn_model.PEPS_model_reuse):
             try:
                 if self.vstate_func.debug:
                     amp.backward(retain_graph=retain_graph)
@@ -126,7 +126,8 @@ class Variational_State:
                 else:
                     self.vstate_func.get_grad()
                     vec_log_grad = self.vstate_func.params_grad_to_vec()/amp
-            except Exception:
+            except Exception as e:
+                print(f"Rank {RANK} model grad debug fail: {e}")
                 raise ValueError("model grad debug fail")
 
             if DEBUG: